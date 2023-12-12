ad5a8db28871672c63165560af521290c37682bd
blakelyons <blake@blakelyons.com>
2023-12-12 17:24:47 -0500

    Fixed issue wiath packages and image paths

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,10 @@ import {fileURLToPath, URL} from "node:url";
 import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
-import ViteAssetsPlugin from "vite-plugin-assets";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        vuetify({styles: {configFile: "src/assets/sass/_settings.scss"}}),
-        ViteAssetsPlugin({
-            // Specify the asset directory
-            include: ["assets/**"],
-        }),
-        npm,
-    ],
+    plugins: [vue(), vuetify({styles: {configFile: "src/assets/sass/_settings.scss"}})],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -28,8 +19,5 @@ export default defineConfig({
     //         },
     //     },
     // },
-    build: {
-        assetsDir: "assets",
-    },
     base: "./", // Set Base URL Path
 });
